fcaffaefbfba10884306524e869f639a16f442c2
FranccoL <leofranco96@gmail.com>
2024-12-18 11:55:03 -0300

    Feat material-ui-insert

--- sales-dashboard-lf/src/pages/Login.tsx
+++ sales-dashboard-lf/src/pages/Login.tsx
@@ -1,31 +1,25 @@
-import styled from 'styled-components'
-import { Box, Container, Grid2 } from '@mui/material'
+import { Box, Container, Grid } from '@mui/material'
+import { BannerImage } from '@/components'
 
-const LoginArea = styled.div`
-  background: #666;
-`
-
-const LoginImage = styled.div`
-  background-image: url(/login-image.svg);
-  background-size: cover;
-  height: 100vh;
-  width: 50vw;
-`
 function Login() {
   return (
     <>
       <Box>
-        <Grid2
-          container
-          sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}
-        >
-          <Container maxWidth="sm">
-            <LoginArea>LOGIN</LoginArea>
-          </Container>
-        </Grid2>
-        <Grid2 container sx={{ display: { xs: 'none', sm: 'block' } }}>
-          <LoginImage />
-        </Grid2>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ display: 'flex', alignItems: 'center', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <h1>LOGIN</h1>
+            </Container>
+          </Grid>
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <BannerImage />
+          </Grid>
+        </Grid>
       </Box>
     </>
   )
